71681460e1bd5e33be64e3b2cc0b5ccb24d89887
James Robb <43144223+JamesRobb03@users.noreply.github.com>
2021-10-05 11:25:19 +0100

    Session ID now in database. Updated Dashboard to cope with backend changes

--- dashboard.php
+++ dashboard.php
@@ -44,6 +44,7 @@
 	    	   echo "<p>".$row3["firstName"]." ".$row3["surname"]."</p>";
 
 			}
+<<<<<<< Updated upstream
 		}
 	?>
 
@@ -80,6 +81,54 @@
 
 	<script src="js/project.js" charset="utf-8"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+=======
+		?>
+		</div>
+
+		
+		<hr>
+		<h1 class="helloName"> Your <span>recovery</span> tracker </h1>
+		<hr>
+
+		<!-- background for the app -->
+		<div class="body_background">
+		<!-- contains all the main content of the page -->
+		<div class="content_container">
+			<!-- test form for importing csv using php -->
+			<div title="form run on php" class="php_form">
+			<form class="row g-3" enctype="multipart/form-data" action="upload.php" method="post">
+				<!-- <div class="col-md-2">
+					<label for="sensor" class="form-label">Choose a sensor:</label>
+					<select id="sensor" name="sensor" class="form-control">
+						<option value="">-----Choose Sensor-----</option>
+						<option value="1">Sensor 1</option>
+						<option value="2">Sensor 2</option>
+						<option value="3">Sensor 3</option>
+						<option value="4">Sensor 4</option>
+					</select>
+				</div> -->
+				<div class="col-12">
+					<label for="file" class="form-label" >Upload Sensor 1 File:</label>
+					<input type="file" class="form-control" id="file_to_import" name="csv_file[]" accept=".csv" required>
+					<br>
+					<label for="file" class="form-label" >Upload Sensor 2 File:</label>
+					<input type="file" class="form-control" id="file_to_import" name="csv_file[]" accept=".csv" required>
+					<br>
+					<label for="file" class="form-label" >Upload Sensor 3 File:</label>
+					<input type="file" class="form-control" id="file_to_import" name="csv_file[]" accept=".csv" required>
+					<br>
+					<label for="file" class="form-label" >Upload Sensor 4 File:</label>
+					<input type="file" class="form-control" id="file_to_import" name="csv_file[]" accept=".csv" required>
+				</div>
+				<input class="form-control" type="submit" id="submitFile" value="Upload File" name="import">
+			</form>
+			<br>
+			<button class="viewLastSession" onclick="window.location.href='3d.php'">View your last session</button>
+			</div>
+		</div>
+
+		</div>
+>>>>>>> Stashed changes
 
 
 </body>
